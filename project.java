424c19efb191ec8206e357e2a32ce7d2c45cb9e8
Anastasiia <nstyaqs@gmail.com>
2024-03-14 00:01:40 +0200

    A method with conversion to binary representation has been created.

--- project.java
+++ project.java
@@ -3,6 +3,46 @@ public class project{
         String input = "2 10 0";
         String[] numbers = input.split(" ");
 
-        
+        String[] binaryNumbers = toBin(numbers);
+
+    }
+
+    public static String[] toBin(String[] numbers){
+        String[] binNumbers = new String[numbers.length];
+        for(int i=0; i< numbers.length; i++){
+            int startNum =0;
+            int sign = 1;
+
+            String inputNum = numbers[i];
+            for(int j=0; j< inputNum.length(); j++){ 
+                char c = inputNum.charAt(j);
+                if (c== '-' ) {
+                    sign = -1;
+                } else {
+                    int digit = Character.getNumericValue(c);
+                    startNum = startNum*10+digit;
+                }
+            }
+
+            startNum *= sign;
+
+            StringBuilder result = new StringBuilder();
+            if (startNum ==0) {
+                result.append("0");
+            } else{
+                while (startNum !=0) {
+                    int remainder = Math.abs(startNum %2);
+                    result.append(remainder);
+                    startNum /= 2;
+                }
+            }
+
+            if (sign == -1) {
+                result.append("-");
+            }
+
+            binNumbers[i] = result.reverse().toString();
+        }
+        return binNumbers;
     }
 }
